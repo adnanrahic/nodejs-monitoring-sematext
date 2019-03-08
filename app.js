a4a08390ee02a3c5841f7b7769b721a149b38d4b
adnanrahic <ado.raha198@gmail.com>
2019-03-08 14:59:58 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+require('dotenv').config()
+require('spm-agent-nodejs')
+const express = require('express')
+const app = express()
+const winston = require('winston')
+const morgan = require('morgan')
+const json = require('morgan-json')
+const format = json(':method :url :status :res[content-length] :response-time')
+const Logsene = require('winston-logsene')
+const logger = winston.createLogger({
+  transports: [new Logsene({
+    token: process.env.LOGS_TOKEN,
+    level: 'info',
+    type: 'api_logs',
+    url: 'https://logsene-receiver.sematext.com/_bulk'
+  })]
+})
+
+app.use(morgan(format, {
+  stream: {
+    write: (message) => {
+      const data = JSON.parse(message)
+      logger.info('HTTP LOG', {
+        method: data.method,
+        url: data.url,
+        status: data.status,
+        responseTime: data['response-time'],
+        contentLength: data.res
+      })
+    }
+  }
+}))
+
+app.get('/api', (req, res, next) => {
+  logger.info('info', 'Api Works.')
+  res.status(200).send('Api Works.')
+})
+
+app.get('/api/fast', (req, res, next) => {
+  res.status(200).send('Fast response!')
+})
+
+app.get('/api/slow', (req, res, next) => {
+  setTimeout(() => {
+    res.status(200).send('Slow response...')
+  }, 1000)
+})
+
+app.get('/api/error', (req, res, next) => {
+  try {
+    throw new Error('Something broke...')
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
+
+app.listen(3000, () => console.log('Server is running on port 3000'))
