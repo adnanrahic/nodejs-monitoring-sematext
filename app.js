2eff571ec5ee319f0885be7ccc75543026e49953
adnanrahic <ado.raha198@gmail.com>
2019-04-28 17:10:17 +0200

    cleanup

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 require('dotenv').config()
 
-require("appdynamics").profile({
- controllerHostName: 'painted2019042202341410.saas.appdynamics.com',
- controllerPort: 443,
+require('appdynamics').profile({
+  controllerHostName: 'painted2019042202341410.saas.appdynamics.com',
+  controllerPort: 443,
 
- // If SSL, be sure to enable the next line
- controllerSslEnabled: true,
- accountName: 'painted2019042202341410',
- accountAccessKey: 'wauxohuzth4k',
- applicationName: 'testnodejs',
- tierName: 'testtier',
- nodeName: 'process' // The controller will automatically append the node name with a unique number
+  // If SSL, be sure to enable the next line
+  controllerSslEnabled: true,
+  accountName: 'painted2019042202341410',
+  accountAccessKey: 'wauxohuzth4k',
+  applicationName: 'testnodejs',
+  tierName: 'testtier',
+  nodeName: 'process' // The controller will automatically append the node name with a unique number
 })
 
 // Bugsnag
@@ -24,29 +24,29 @@ const Sentry = require('@sentry/node')
 Sentry.init({ dsn: 'https://0a0e82ea1bd44b22b4e4be82d0097cbd@sentry.io/1444800' })
 
 // Sematext
-require ('spm-agent-nodejs')
+require('spm-agent-nodejs')
 const tracer = require('dd-trace').init()
 const express = require('express')
 const app = express()
 
 const middleware = bugsnagClient.getPlugin('express')
 
-const StatsD = require('node-dogstatsd').StatsD;
-const dogstatsd = new StatsD();
+const StatsD = require('node-dogstatsd').StatsD
+const dogstatsd = new StatsD()
 
 const opentracing = require('opentracing')
 opentracing.initGlobalTracer(tracer)
 
-const winston = require('winston');
-const LogzioWinstonTransport = require('winston-logzio');
+const winston = require('winston')
+const LogzioWinstonTransport = require('winston-logzio')
 const logzioWinstonTransport = new LogzioWinstonTransport({
   level: 'info',
   name: 'winston_logzio',
   token: 'ZVWKrnJWjmxMORRJrRVkLUJfLQqJeYHP',
-  host: 'listener.logz.io',
-});
+  host: 'listener.logz.io'
+})
 const logger = winston.createLogger({
-    transports: [logzioWinstonTransport]
+  transports: [logzioWinstonTransport]
 })
 
 app.use(middleware.requestHandler)
@@ -54,7 +54,7 @@ app.use(Sentry.Handlers.requestHandler())
 
 app.get('/api', (req, res, next) => {
   // console.log('Api Works.')
-  logger.log('warn', 'Just a test message', new Error('Big problem'));
+  logger.log('warn', 'Just a test message', new Error('Big problem'))
   dogstatsd.increment('page.views')
   bugsnagClient.notify('Testing Bugsnag.')
   Sentry.captureMessage('Testing Sentry.')
@@ -79,4 +79,3 @@ app.use(middleware.errorHandler)
 app.use(Sentry.Handlers.errorHandler())
 
 module.exports = app
-
--- cluster.js
+++ cluster.js
@@ -27,6 +27,5 @@ function masterProcess () {
 function childProcess () {
   console.log(`Worker ${process.pid} started...`)
 
-  app.listen(3000, () => console.log('Server is running on port 3000'))  
+  app.listen(3000, () => console.log('Server is running on port 3000'))
 }
-
